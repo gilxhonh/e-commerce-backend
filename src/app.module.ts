a1f1d995d269bd9ddd653a21ce57656dae483924
Gilxhon Hima <gilxhonhima@gmail.com>
2024-01-28 22:44:30 +0100

    Configure environment variables for database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
+import { Variant } from './entities/variant.entity';
+import { Product } from './entities/product.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes ConfigModule globally available
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres', // type of our database
-      host: 'localhost', // database host
-      port: 5432, // database port
-      database: 'postgres', // name of our database
+      host: process.env.DATABASE_HOST, // database host
+      port: parseInt(process.env.DATABASE_PORT, 10), // database port
+      database: process.env.DATABASE_NAME, // name of our database
+      synchronize: process.env.NODE_ENV !== 'production', // Avoid using synchronize in production
       autoLoadEntities: true, // models will be loaded automatically (recommended)
-      synchronize: true, // your entities will be synced with the database (use cautiously in production)
+      entities: [Product, Variant], // specify entities here
     }),
+    ProductsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
