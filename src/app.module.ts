40dfd7e61190f14e75f4514b8967422e353824a3
Gilxhon Hima <gilxhonhima@gmail.com>
2024-01-28 21:47:45 +0100

    configure TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres', // type of our database
+      host: 'localhost', // database host
+      port: 5432, // database port
+      database: 'postgres', // name of our database
+      autoLoadEntities: true, // models will be loaded automatically (recommended)
+      synchronize: true, // your entities will be synced with the database (use cautiously in production)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
